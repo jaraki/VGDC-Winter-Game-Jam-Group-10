ba93e26c1351ed8d4f249b511564851cdd696d97
Nick Smith <smithne@uci.edu>
2016-01-16 14:18:54 -0800

    added fred stuff
    scripts and sprites

---
+++ Assets/Scripts/StaminaPickup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour {
+
+	public float staminaAmount;
+	public AudioClip collected;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+//			PlayerStamina playerStamina = other.GetComponent<PlayerStamina>();
+//			
+//			playerStamina.stamina += staminaAmount;
+//			playerStamina.stamina = Mathf.Clamp(playerHealth.stamina, 0f, 100f);
+//			
+//			playerStamina.UpdateStaminaBar();
+			
+			Destroy(transform.root.gameObject); 
+			
+		}
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TimePickup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Time : MonoBehaviour {
+
+
+	public float timeAmount;
+	public AudioClip collected;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+//			PlayerTime playerTime = other.GetComponent<PlayerTime>();
+//			
+//			playerTime.time += timeAmount;
+//			playerTime.time = Mathf.Clamp(playerTime.time, 0f, 100f);
+//			
+//			playerTime.UpdateTimeBar();
+			
+			Destroy(transform.root.gameObject); 
+			
+		}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
