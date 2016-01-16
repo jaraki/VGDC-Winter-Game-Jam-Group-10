0786ce6ad139336fa78ca6afc1322a37243db949
Hyperlazydude <AnimeAzn101@gmail.com>
2016-01-15 23:21:41 -0800

    Scene

---
+++ Assets/Scripts/menu_buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu_buttons : MonoBehaviour {
+
+    public void playGame(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void controlGame(string controlName)
+    {
+        Application.LoadLevel(controlName);
+    }
+}
