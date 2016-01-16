0df848203db3d1b1528a3a751b29e50f897af4ac
jaraki <arakij@uci.edu>
2016-01-16 15:18:22 -0800

    Integrated items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+    public string type;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        tag = type;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
