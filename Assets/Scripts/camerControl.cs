a68ee3d9a1db5618e3e50843cd5ddbd26b7340f5
Hyperlazydude <AnimeAzn101@gmail.com>
2016-01-16 15:55:49 -0800

    Setup Camera Control

---
+++ Assets/Scripts/camerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerControl : MonoBehaviour {
+    public Movement thePlayer;
+
+    private Vector3 lastPoisition;
+    private float distanceTravelX;
+    private float distanceTravelY;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<Movement>();
+        lastPoisition = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distanceTravelX = thePlayer.transform.position.x - lastPoisition.x;
+        distanceTravelY = thePlayer.transform.position.y - lastPoisition.y;
+        transform.position = new Vector3(transform.position.x + distanceTravelX, transform.position.y + distanceTravelY, transform.position.z);
+        lastPoisition = thePlayer.transform.position;
+	
+	}
+}
