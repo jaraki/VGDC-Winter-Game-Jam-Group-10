b1a163ada5e4b1f31d8fba2009fe6f4e8a642aca
NicodySazawa <cbsauer@uci.edu>
2016-01-16 23:28:51 -0800

    added teleport script

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float teleportY;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.CompareTag("Player"))
+		{
+			rb = col.gameObject.GetComponent<Rigidbody2D>();
+
+			rb.MovePosition (new Vector2(rb.position.x, rb.position.y + teleportY));
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
