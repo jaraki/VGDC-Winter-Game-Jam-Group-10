d9f99cc779ad5b2c15f1645c7b1bcba90e588022
jaraki <arakij@uci.edu>
2016-01-17 22:45:48 -0800

    Added menu and hooked everything up

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
