97985cf46e32564262e149d2bdcdc18fa96da32e
jaraki <arakij@uci.edu>
2016-01-28 21:00:16 -0800

    Initial Push

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private float maxHealth;
+    private float currentHealth;
+    private float maxMana;
+    private float currentMana;
+    private float maxStamina;
+    private float currentStamina;
+    private float maxTime;
+    private float currentTime;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
